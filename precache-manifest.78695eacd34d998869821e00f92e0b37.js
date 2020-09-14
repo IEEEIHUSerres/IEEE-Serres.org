25cce834f804dbd39b74df0aaa62c9397ad6f09c
Iordanis Kostelidis <kostelidis@ieee.org>
2020-09-14 21:06:06 +0300

    Updates

---
+++ precache-manifest.78695eacd34d998869821e00f92e0b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19105f0d5c438fb3be25a2e51d26136b",
+    "url": "/IEEE-Serres.org/index.html"
+  },
+  {
+    "revision": "d2d8b26938f46556f078",
+    "url": "/IEEE-Serres.org/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cbc87f7119236db3e22c",
+    "url": "/IEEE-Serres.org/static/js/2.1b768444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IEEE-Serres.org/static/js/2.1b768444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d8b26938f46556f078",
+    "url": "/IEEE-Serres.org/static/js/main.e4ea68af.chunk.js"
+  },
+  {
+    "revision": "a5b295d72adf4c5bd770",
+    "url": "/IEEE-Serres.org/static/js/runtime-main.31a32af9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/IEEE-Serres.org/static/media/logo.5d5d9eef.svg"
+  }
+]);
