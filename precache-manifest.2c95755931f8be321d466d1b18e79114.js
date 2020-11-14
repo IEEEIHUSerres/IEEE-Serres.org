142f79f3efa7c9056b9ab6293bf410308ea91050
Iordanis Kostelidis <kostelidis@ieee.org>
2020-11-14 20:11:59 +0200

    Updates

---
+++ precache-manifest.2c95755931f8be321d466d1b18e79114.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a11f0feecfc9246fde4e1ada1958d3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e701fe6e4dc95892c46e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cbc87f7119236db3e22c",
+    "url": "/static/js/2.1b768444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b768444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e701fe6e4dc95892c46e",
+    "url": "/static/js/main.13239c05.chunk.js"
+  },
+  {
+    "revision": "bb6c5badedce47c68f41",
+    "url": "/static/js/runtime-main.2023949c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
