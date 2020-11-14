e1d8cc2dd900f70459b29e1e062c7840d9b39df0
Iordanis Kostelidis <kostelidis@ieee.org>
2020-11-14 22:29:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c95755931f8be321d466d1b18e79114.js"
+  "/precache-manifest.5862af62100dfa5eace9e3c9dd916a17.js"
 );
 
 self.addEventListener('message', (event) => {
